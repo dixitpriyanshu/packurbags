6559e51a6379984ed090b6b44b2634866b483785
Hitha Shri <hithashrinagaruru@gmail.com>
2023-01-14 15:25:50 +0530

    first commit

---
+++ packurbags/store/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import json
+
+from .models import *
+
+# Create your views here.
+
+
+def store(request):
+    packages = Package.objects.all()
+    context = {'packages':packages}
+    return render(request, 'store/store.html', context)
+
+
+def cart(request):
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order , created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0 }
+    context = {'items':items , 'order':order}
+    return render(request, 'store/cart.html', context)
+
+
+def checkout(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order , created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0 }
+    context = {'items':items , 'order':order}
+    return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+	data = json.loads(request.body)
+	packageId = data['packageId']
+	action = data['action']
+	print('Action:', action)
+	print('PackageId:', packageId)
+
+	# customer = request.user.customer
+	# package = Package.objects.get(id=packageId)
+	# order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+	# orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+	# if action == 'add':
+	# 	orderItem.quantity = (orderItem.quantity + 1)
+	# elif action == 'remove':
+	# 	orderItem.quantity = (orderItem.quantity - 1)
+
+	# orderItem.save()
+
+	# if orderItem.quantity <= 0:
+	# 	orderItem.delete()
+
+	return JsonResponse('Item was added', safe=False)
